5301e041780d2361c4167c27354da48893abda44
Collin <collinhodgs@gmail.com>
2020-09-24 18:30:13 -0600

    Added router.get in api.js

--- routes/api.js
+++ routes/api.js
@@ -24,3 +24,13 @@ router.put("/api/workouts/:id", ({ body, params }, res) => {
       res.json(err);
     });
 });
+
+router.get("/api/workouts", (req, res) => {
+  Workout.find()
+    .then((dbWorkouts) => {
+      res.json(dbWorkouts);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
