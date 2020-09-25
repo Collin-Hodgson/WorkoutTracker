a54ee989cde2a1afcf8a3b58417fb823fb7ce601
Collin <collinhodgs@gmail.com>
2020-09-24 21:10:29 -0600

    Started models files and folder

---
+++ models/workout.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: () => new Date()
+    },
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise type"
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise name"
+        }
+    }
+]
+}),
