2bfb3304c414228ff4b31126db1f6dd3ec3dc05a
Collin <collinhodgs@gmail.com>
2020-09-26 21:58:02 -0600

    Reworked api.js

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,35 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
+router.post("/api/workouts", ({ body }, res) => {
+  Workout.create(body)
+    .then((dbWorkouts) => {
+      res.json(dbWorkouts);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts", (req, res) => {
+  Workout.find({})
+    .then((dbWorkouts) => {
+      res.json(dbWorkouts);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+    .limit(7)
+    .then((dbWorkouts) => {
+      res.json(dbWorkouts);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
 module.exports = router;
